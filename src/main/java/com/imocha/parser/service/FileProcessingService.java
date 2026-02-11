a656bee2308ffa74d8062bd6ed60f021671459e3
Saber PourmomenKangacher <saber.pourmomen@gmail.com>
2026-02-12 07:12:47 +0800

    make exceptions readable

--- src/main/java/com/imocha/parser/service/FileProcessingService.java
+++ src/main/java/com/imocha/parser/service/FileProcessingService.java
@@ -8,7 +8,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.JobParametersInvalidException;
 import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StreamUtils;
 import org.springframework.web.multipart.MultipartFile;
@@ -17,6 +21,7 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
@@ -40,10 +45,13 @@ public class FileProcessingService {
                 if (!FileNameValidator.isValidate(entry)) {
                     continue;
                 }
-                log.info("{} Start Processing File:{}{}","*".repeat(8),entry.getName(),"*".repeat(8));
-                Path tempFile = Files.createTempFile(
-                        "call_", "_" + entry.getName()
-                );
+
+                String fileName = Paths.get(entry.getName())
+                        .getFileName()
+                        .toString();
+                log.info("{} Start Processing File: {} {}",
+                        "*".repeat(8), fileName, "*".repeat(8));
+                Path tempFile = Files.createTempFile("call_", "_" + fileName);
 
                 try (OutputStream os = Files.newOutputStream(tempFile)) {
                     StreamUtils.copy(zis, os);
@@ -51,15 +59,34 @@ public class FileProcessingService {
 
                 JobParameters jobParameters = new JobParametersBuilder()
                         .addString("filePath", tempFile.toAbsolutePath().toString())
-                        .addString("fileName", entry.getName())
+                        .addString("fileName", fileName)
                         .addLong("timestamp", System.currentTimeMillis())
                         .toJobParameters();
 
-                jobLauncher.run(importCallJob, jobParameters);
+                launchJob(jobParameters, fileName);
+                log.info("{} processing done for: {} {}",
+                        "*".repeat(8), fileName, "*".repeat(8));
             }
 
-        } catch (Exception e) {
-            throw new JobExecutionException("Failed to process ZIP file",e);
+        }
+    }
+
+    private void launchJob(JobParameters jobParameters, String fileName) {
+
+        try {
+            jobLauncher.run(importCallJob, jobParameters);
+
+        } catch (JobExecutionAlreadyRunningException |
+                 JobRestartException |
+                 JobInstanceAlreadyCompleteException |
+                 JobParametersInvalidException e) {
+
+            log.error("Batch job failed for file: {}", fileName, e);
+
+            throw new JobExecutionException(
+                    "Batch execution failed for file: " + fileName,
+                    e
+            );
         }
     }
 
